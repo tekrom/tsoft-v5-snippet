55cc96d8ab17fde4644f294ea1caf07aeb3baf67
huseyin <huseyindasci.tr@gmail.com>
2022-05-12 15:32:14 +0300

    some extra file added

---
+++ src/extension.js
@@ -0,0 +1,6 @@
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+}
+exports.activate = activate;
+//# sourceMappingURL=extension.js.map
