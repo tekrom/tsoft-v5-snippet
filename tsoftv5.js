7163763bcd02212b772ceb8d882deef69a5ec08d
huseyin <huseyindasci.tr@gmail.com>
2022-05-17 15:35:32 +0300

    js files added accept for services

--- tsoftv5.js
+++ tsoftv5.js
@@ -1,14 +1,3 @@
-A long work, just for the developers to read this file
-# Tsoft V5 javascript 
-Tsoft v5 smarty javascript and scss snippets
-# Snippets
-TSoft V5 Javascript snippet
-## Source
-[MIT](https://github.com/gitorhub/tsoft-v5-snippet/blob/main/README.md)
-## License
-[MIT](https://github.com/gitorhub/tsoft-v5-snippet/blob/main/LICENCE)
-## javacript
-```javascript
 selector.addEventListener("click",e=>{
     console.log(e)
 }); 
@@ -214,4 +203,7 @@ loadSubFolder({
 });
 T.isMobile()
 
-```
+
+
+////
+
